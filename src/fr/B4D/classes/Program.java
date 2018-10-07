de160af49a72b6a22309f2cee59d3424d7856aa5
MrDomoo <mrdomoo@outlook.fr>
2018-10-07 19:18:57 +0200

    Can now lauch a program

--- src/fr/B4D/classes/Program.java
+++ src/fr/B4D/classes/Program.java
@@ -1,80 +1,148 @@
 package fr.B4D.classes;
 
-import java.awt.Point;
-import java.util.concurrent.Callable;
+import java.awt.AWTException;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import fr.B4D.enu.Category;
 import fr.B4D.enu.Place;
 import fr.B4D.enu.Ressource;
 import fr.B4D.enu.RessourceType;
+import fr.B4D.exceptions.B4DFullInventory;
+import fr.B4D.exceptions.B4DStopProgram;
+import fr.B4D.exceptions.B4DWrongPosition;
+import fr.B4D.modules.B4DKeyboard;
+import fr.B4D.modules.B4DMouse;
+import fr.B4D.modules.B4DOther;
+import fr.B4D.modules.B4DScreen;
+import fr.B4D.programs.Test;
+import fr.B4D.threads.StopThread;
 
-public class Programme extends Thread{
+public class Program extends Thread{
 
 	  /****************/
 	 /** COLLECTION **/
 	/****************/
 	
+	public final static Program test = new Program(Place.Aucune, Category.Aucune, RessourceType.Aucun, Ressource.Aucune, Test.test, 0, 0);
 	
-	
-	  /*************/
-	 /* ATTRIBUTS */
-	/*************/
+	  /***************/
+	 /** ATTRIBUTS **/
+	/***************/
 	
 	private Place place;
 	private Category category;
 	private RessourceType type;
 	private Ressource ressource;
 
-	private Point start;
-	private Callable<Integer> fonction;
+	private ProgramInterface program;
 	
 	private int maxCycles;
-	private int maxSubmissions;
+	private int maxDeposits;
 
-	  /****************/
-	 /* CONSTRUCTEUR */
-	/****************/
+	  /******************/
+	 /** CONSTRUCTEUR **/
+	/******************/
 	
-	public Programme(Place lieux, Category categorie, RessourceType type, Ressource ressource, Callable<Integer> fonction, Point depart, int nombre_tour, int nombre_depot) {
-		this.place = lieux;
-		this.category = categorie;
+	public Program(Place place, Category category, RessourceType type, Ressource ressource, ProgramInterface program, int maxCycles, int maxDeposits) {
+		this.place = place;
+		this.category = category;
 		this.type = type;
 		this.ressource = ressource;
-		this.start = depart;
-		this.fonction = fonction;
-		this.maxCycles = nombre_tour;
-		this.maxSubmissions = nombre_depot;
+		this.program = program;
+		this.maxCycles = maxCycles;
+		this.maxDeposits = maxDeposits;
 	}
 	
-	  /*********************/
-	 /* GETTERS & SETTERS */
-	/*********************/
+	  /************************/
+	 /** METHODES STATIQUES **/
+	/************************/
 	
-	//A faire si besoin
+  public final static ArrayList<Program> getAll(){
+  	ArrayList<Program> programs = new ArrayList<Program>();
+  	programs.add(test);
+    return programs;
+  }
+      
+  public final static Program getProgram(int index){
+	  return getAll().get(index);
+  }
 	
-	  /************/
-	 /* METHODES */
-	/************/
+	  /***********************/
+	 /** GETTERS & SETTERS **/
+	/***********************/
+	
+	public Place getPlace() {
+		return place;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public RessourceType getType() {
+		return type;
+	}
+	public Ressource getRessource() {
+		return ressource;
+	}
+	
+	  /**************/
+	 /** METHODES **/
+	/**************/
 	
 	public void run() {
+		StopThread stopThread = new StopThread(this);	//Thread permettant de gérer le thread principal
+		stopThread.start();								//Demarre le thread
 		try {
 			Intro();
-			while (Tour() == true);
+			Tours();
+		}catch(B4DWrongPosition | AWTException | UnsupportedFlavorException | IOException e){
 			Outro();
-		}catch(Exception e){
-			e.printStackTrace();
-		}finally {
+			e.getStackTrace().toString();
+			//Message mal fini
+		}catch(B4DStopProgram e){
+			Outro();
+			//Message correctement fini
 		}
+		stopThread.interrupt();							//Arrete le thread
 	}
-	private void Intro() throws Exception{
+
+	private void Intro() throws B4DWrongPosition, AWTException, UnsupportedFlavorException, IOException{
 		
+		B4DOther.focusDofus();
+		B4DKeyboard.writeChat("/clear");
+		
+		if(B4DScreen.getPixelColor(new PointF(0.28, 0.99)).getGreen() > 200){	//Le mode solo n'est pas activé
+            B4DMouse.leftClick(new PointF(0.28, 0.99), false);                	//Clic sur status
+            B4DMouse.leftClick(new PointF(0.3, 0.976), false);                	//Clic sur solo
+		}
+
+		Bot.configuration.persons.get(0).position = Bot.world.getPosition();	//Ma position théorique devient la position réelle
 	}
-	private boolean Tour() {
-		
-		//fonction.call()
-		return true;
+	private void Tours() throws B4DStopProgram{
+		while(maxCycles != 0 && maxDeposits != 0) {
+			try {
+				program.run();
+			} catch (B4DFullInventory e) {			
+				
+				if(Bot.configuration.hdvWhenFull) {
+					//Mettre en HDV
+				}
+				if(Bot.configuration.bankWhenFull) {
+					//Mettre en banque
+				}
+				if(Bot.configuration.stopWhenFull)
+					throw new B4DStopProgram();
+				
+				maxDeposits = (maxDeposits>0 ? maxDeposits-1 : maxDeposits);	//Décrémente le nombre de depots si non infini
+				
+			}finally {
+				maxCycles = (maxCycles>0 ? maxCycles-1 : maxCycles);			//Décrémente le nombre de cycles si non infini	
+			}
+		}
+		throw new B4DStopProgram();
 	}
 	private void Outro() {
-		
+		B4DOther.focusBot();
 	}
 }
---
+++ src/fr/B4D/classes/ProgramInterface.java
@@ -0,0 +1,7 @@
+package fr.B4D.classes;
+
+import fr.B4D.exceptions.B4DFullInventory;
+
+public interface ProgramInterface {
+	public void run() throws B4DFullInventory;
+}
