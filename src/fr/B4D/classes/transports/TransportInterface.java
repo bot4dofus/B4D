29f398d9f76cfcee29f186347560f6c9487ab4db
MrDomoo <mrdomoo@outlook.fr>
2018-10-15 19:29:28 +0200

    Create the main world, patch zaaps and zaapis

--- src/fr/B4D/classes/transports/TransportInterface.java
+++ src/fr/B4D/classes/transports/TransportInterface.java
@@ -6,5 +6,13 @@ import java.awt.Point;
 import fr.B4D.exceptions.B4DCannotFind;
 
 public interface TransportInterface {
+	final double zaapCost = 10;
+	final double zaapiBontaCost = 1;
+	final double zaapiBrakmarCost = 1;
+	final double boosterPotionCost = 3;
+	final double bontaPotionCost = 3;
+	final double brakmarPotionCost = 3;
+	final double almanaxPotionCost = 5;
+
 	public void goTo(Point destination) throws AWTException, B4DCannotFind;
 }
--- src/fr/B4D/programs/Test.java
+++ src/fr/B4D/programs/Test.java
@@ -1,33 +1,19 @@
 package fr.B4D.programs;
 
 import java.awt.Point;
-import java.util.List;
 
+import fr.B4D.classes.Bot;
 import fr.B4D.classes.ProgramInterface;
-import fr.B4D.classes.transports.B4DEdge;
-import fr.B4D.classes.transports.B4DGraph;
-import fr.B4D.enu.TransportType;
 
 public final class Test {
 	public static ProgramInterface test = new ProgramInterface() {
 		public void run() {
-			B4DGraph graph = new B4DGraph();
-			graph.addVertex(new Point(1,1));
-			graph.addVertex(new Point(2,2));
-			graph.addVertex(new Point(3,3));
-	
-		    graph.addB4DEdge(new Point(1,1), new Point(3,3), 6, TransportType.Marche);
-		    graph.addB4DEdge(new Point(1,1), new Point(2,2), 3, TransportType.Zaap);
-		    graph.addB4DEdge(new Point(2,2), new Point(3,3), 2, TransportType.Zaap);
-		    
-		    List<B4DEdge> shortestPath = graph.getPath(new Point(1,1), new Point(3,3));
-		    System.out.println(shortestPath);
-		    
-		   /* try {
-				System.out.println(Bot.world.getPosition().toString());
-			} catch (AWTException | UnsupportedFlavorException | IOException e) {
+			Bot.configuration.persons.get(0).position = new Point(0, 0);
+			try {
+				Bot.world.goTo(new Point(-30,-57));
+			} catch (Exception e) {
 				e.printStackTrace();
-			}*/
+			}
 		}
 	};
 }
