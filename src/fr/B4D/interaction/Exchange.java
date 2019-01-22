944b5fb88403afc8b2f4112599a6ff4e15a68bd8
LucBerge <lucas.bergeron@outlook.fr>
2019-01-22 12:32:09 +0100

    Changed the way exchanges works

--- src/fr/B4D/interaction/Exchange.java
+++ src/fr/B4D/interaction/Exchange.java
@@ -6,6 +6,10 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.Serializable;
 
+import fr.B4D.bot.B4D;
+import fr.B4D.dofus.Dofus;
+import fr.B4D.interaction.chat.Channel;
+import fr.B4D.interaction.chat.Message;
 import fr.B4D.modules.B4DChat;
 import fr.B4D.modules.B4DKeyboard;
 import fr.B4D.modules.B4DMouse;
@@ -19,19 +23,19 @@ public class Exchange implements Serializable{
 
 	private static final long serialVersionUID = 6230637950441806082L;
 	
-	/***************/
+	  /***************/
 	 /** CONSTANTS **/
 	/***************/
 	
-	private final String waitForExchangeKey = "souhaite faire"; 	//clef permettant de valider l'echange
-	private final int waitForExchangeTimeout = 60; 					//Temps avant chaque vérification de demande d'échange
+	private static String[] validationKeys = {"oui","Oui","OUI","ui","Ui","yes","Yes","YES", "yep", "Yep", "YEP"};
+	
 	private final Rectangle waitForExchangeRectangle = new Rectangle(new PointF(0.3762, 0.4756), new PointF(0.6238, 0.5055)); //Zone des kamas entrants
 	private final PointF waitForExchangeYesButton = new PointF(0.4143, 0.5304);		//Emplacement du bouton accepter
 	
-	private final int iterationNumber = 2; 			//Nombre de fois que l'instruction d'échange est envoyée
-	private final int iterationTimeout = 30; 		//Temps avant chaque répétition d'instruction
+	private final int timeout = 30000; 	//Timeout avant echec de l'échange
 	private final Rectangle kamasInRectangle = new Rectangle(new PointF(0.2163, 0.2562), new PointF(0.2835, 0.2742)); //Zone somme entrante
 
+	private final PointF escapeButton = new PointF(x, x);				//Emplacement de la croix
 	private final PointF validationButton = new PointF(0.3491, 0.5334);		//Emplacement du bouton de validation de l'echange
 	
 	  /**************/
@@ -50,53 +54,106 @@ public class Exchange implements Serializable{
 		this.kamasOut = kamasOut;
 	}
 	
+	  /*************/
+	 /** GETTERS **/
+	/*************/
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		synchronized(this.name) {
+			this.name = name;
+			name.notifyAll();
+		}
+	}
+	
 	  /**************/
 	 /** METHODES **/
 	/**************/
 	
-	public String waitForExchange(String welcomMessage) throws AWTException, IOException, TesseractException {
-		String message;
-		while((message = B4DWait.waitForOCR(waitForExchangeRectangle, waitForExchangeKey, waitForExchangeTimeout)) == null);
-		String name = message.split(" ")[0];
-		B4DMouse.leftClick(waitForExchangeYesButton, false);
-		B4DChat.sendPrivateMessage(name, welcomMessage);
+	/** Permet d'attendre un échange provoqué par un joueur.
+	 * @return le nom du joueur ayant demandé l'échange
+	 * @throws AWTException si impossible de valider l'échange
+	 */
+	public String waitForExchange() throws AWTException {
+		return waitForExchange(0);
+	}
+
+	/** Permet d'attendre un échange provoqué par un joueur.
+	 * @param timeout le nombre de millisecondes à attendre avant timeout
+	 * @return le nom du joueur ayant demandé l'échange. {@code null} si timeout
+	 * @throws AWTException si impossible de valider l'échange
+	 */
+	public String waitForExchange(long timeout) throws AWTException {
+		if(!B4D.getSocketListener().isAlive())
+			B4D.getSocketListener().start();
+		
+		Message message;
+		try {
+			synchronized(name){
+				if(name == null) 
+					name.wait(timeout);
+				if(name != null)
+					B4DMouse.leftClick(waitForExchangeYesButton, false);
+			}
+		}
+		catch(InterruptedException e) {
+			B4D.logger.error(e);
+		}
 		return name;
 	}
 	
-	public void exchange(String exchangeMessage) throws B4DExchangeCanceled, AWTException, NumberFormatException, IOException, TesseractException {
-		//Si kamas out > 0
-			//Ajout des kamas out
-		for(int i=1;i<=iterationNumber;i++) {
-			B4DChat.sendPrivateMessage(name, exchangeMessage);
-			String kamas = B4DWait.waitForOCR(kamasInRectangle, String.valueOf(kamasIn), iterationTimeout);
-			//Attendre la validation du type (vert)
-			if(kamas != null) {
-				if(Integer.valueOf(B4DScreen.OCR(kamasInRectangle)) == kamasIn)
-					break;
-			}
-			if(i == iterationNumber) {
-				cancelExchange();
-			}
+	/**
+	 * @param validationMessage
+	 * @param validationKeys
+	 * @return
+	 * @throws B4DExchangeCanceled si 
+	 */
+	public Image exchange(String validationMessage, String[] validationKeys) throws B4DExchangeCanceled, AWTException, TesseractException, NumberFormatException, IOException {
+		Message message;
+		
+		if(kamasOut > 0) {
+			//Clique sur le champs kamas
+			B4DKeyboard.writeKeyboard(Integer.toString(kamasOut));
 		}
-	}
-	
-	public Image validExchange(String validationMessage, String[] validationKeys) throws AWTException {
-		//B4DChat.sendPrivateMessage(name, validationMessage);
-		//WaitFotChat
-		//si null
-			//throw
-		Image proof = B4DScreen.takeSreenshot();
+		
+		B4DWait.waitForOCR(kamasInRectangle, String.valueOf(kamasIn), timeout);
+		//Attendre la validation (vert) avec timeout
+
+		do {
+			do {				
+				message = new Message(name, Channel.Private, validationMessage);
+				message.send();
+				message = message.waitForReply(timeout);
+				
+				if(message == null)
+					cancelExchange();
+				
+			}while(contains(message.getText(), validationKeys));
+		}while(Integer.parseInt(B4DScreen.OCR(kamasInRectangle)) != kamasIn && ecran vert);
+
+		Image image = B4DScreen.takeSreenshot();
 		B4DMouse.leftClick(validationButton, false);
-		return proof;
+		return image;
 	}
 	
-	public void cancelExchange() throws B4DExchangeCanceled, AWTException {
+	private static boolean contains(String string, String[] keys) {
+		for(String key:keys) {
+			if(string.contains(key))
+				return true;
+		}
+		return false;
+	}
+	
+	private void cancelExchange() throws B4DExchangeCanceled {
 		if(isInProgress())
-			B4DKeyboard.sendKey(KeyEvent.VK_ESCAPE);
+			//Clique sur la croix
 		throw new B4DExchangeCanceled();
 	}
 	
-	public boolean isInProgress() {
+	private boolean isInProgress() {
 		//if window still here
 			//return true;
 		//else
