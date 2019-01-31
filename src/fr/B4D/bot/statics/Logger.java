8ebf5ca1e3f3e50c528f466169bf55fe1471c90e
LucBerge <lucas.bergeron@outlook.fr>
2019-01-31 17:59:35 +0100

    Can now send emails

--- src/fr/B4D/bot/statics/Logger.java
+++ src/fr/B4D/bot/statics/Logger.java
@@ -1,39 +1,49 @@
 package fr.B4D.bot.statics;
 
-import java.awt.Component;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.LineNumberReader;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.swing.JOptionPane;
 
 public class Logger {
 
-	  /**************/
-	 /** ATRIBUTS **/
+	/**************/
+	/** ATRIBUTS **/
 	/**************/
 
 	private final String path = "errors.txt";
-	private final String host = "localhost";
-	private final String to = "b4d@develloper.com";
-	private final String fromFeedback = "feedback@b4d.com";
-	private final String fromRepport = "repport@b4d.com";
-	
-	  /*************/
-	 /** METHODS **/
+	private final String username = "lucas.bergeron@outlook.fr";
+	private final String password = "*****************";
+
 	/*************/
-	
+	/** METHODS **/
+	/*************/
+
 	public void popUp(String log) {
 		debug(this, "Popup showed : " + log);
 		JOptionPane.showMessageDialog(null, log, "Information", JOptionPane.INFORMATION_MESSAGE);
 	}
-	
+
 	public void debug(Object c, String log) {
 		System.out.println("[" + c.getClass().getName() + "] " + log);
 	}
@@ -41,72 +51,103 @@ public class Logger {
 	public void warning(String log) {
 		System.err.println(log);
 	}
-	
+
 	public void error(Exception e) {
 		e.printStackTrace();
 		String message = e.getMessage() + "\n\n=====\n\nVoulez vous envoyer le rappot d'erreur au développeur ?";
 		int answer = JOptionPane.showConfirmDialog(null, message, "Erreur", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
-		
+
 		if (answer == JOptionPane.YES_OPTION) {
 			if(addRepport(e))
-				sendEmail(fromRepport, null, path);
+				sendEmail("Repport B4D", null, path);
 		}
 	}
-	
-	  /*******************/
-	 /** EMAIL METHODS **/
+
+	private int numberOfLines(String path) {
+		int linenumber = 0;
+		try{
+			File file = new File(path);
+			if(file.exists()){
+				FileReader fr = new FileReader(file);
+				LineNumberReader lnr = new LineNumberReader(fr);
+				while (lnr.readLine() != null)
+					linenumber++;
+				lnr.close();
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return linenumber;
+	}
+
+	/*******************/
+	/** EMAIL METHODS **/
 	/*******************/
 
-	private void sendFeedback(String message) {
-		sendEmail(fromFeedback, message, null);
+	public void sendFeedback(String message) {
+		sendEmail("Feedback B4D", message, null);
 	}
-	private boolean addRepport(Exception e) {		
+
+	public boolean addRepport(Exception e) {		
 		try {
 			BufferedWriter writer = new BufferedWriter(new FileWriter(path, true));
 			PrintWriter pw = new PrintWriter(writer);
 			e.printStackTrace(pw);
 			pw.print("\n\n");
 			writer.close();
-
-			URI uri = this.getClass().getResource(path).toURI();
-	        return Files.readAllLines(Paths.get(uri), Charset.defaultCharset()).size() > 100;
-		} catch (IOException | URISyntaxException e1) {
+			return numberOfLines(path) > 100;
+		} catch (IOException e1) {
 			return false;
 		}
 	}
-	
-	
-	
-	private void sendEmail(String from, String message, String path) {
+
+
+
+	public void sendEmail(String subject, String message, String path) {
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", "smtp-mail.outlook.com");
+        properties.put("mail.smtp.port", "587");
+        
+        properties.put("mail.smtp.user", username);
+        properties.put("mail.smtp.password", password);
+        
+        Session session = Session.getInstance(properties,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
 		
-//	      Properties properties = System.getProperties();
-//	      properties.setProperty("mail.smtp.host", host);
-//	      Session session = Session.getDefaultInstance(properties);
-//
-//	      try {
-//	         MimeMessage message = new MimeMessage(session);
-//	         message.setFrom(new InternetAddress(from));
-//	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-//	         message.setSubject("This is the Subject Line!");
-//	         message.setText("This is actual message");
-//	         Transport.send(message);
-//		
-//			if(path != null) {
-//		        Multipart multipart = new MimeMultipart();
-//		        multipart.addBodyPart(messageBodyPart);
-//		
-//		        messageBodyPart = new MimeBodyPart();
-//		        DataSource source = new FileDataSource(path);
-//		        messageBodyPart.setDataHandler(new DataHandler(source));
-//		        messageBodyPart.setFileName(filename);
-//		        multipart.addBodyPart(messageBodyPart);
-//		        message.setContent(multipart );
-//		   }
-//		
-	         debug(this, "Repport sent");
-//	      } catch (MessagingException mex) {
-//	         mex.printStackTrace();
-//	      }
-		
+		try {
+			MimeMessage mail = new MimeMessage(session);
+			mail.setFrom(new InternetAddress(username));
+			mail.addRecipient(Message.RecipientType.TO, new InternetAddress(username));
+			mail.setSubject(subject);
+			
+			Multipart multipart = new MimeMultipart();
+			
+			if(message != null) {
+				BodyPart bodyPart = new MimeBodyPart();
+				bodyPart.setText(message);
+				multipart.addBodyPart(bodyPart);
+			}
+
+			if(path != null) {
+				BodyPart bodyPart = new MimeBodyPart();
+				DataSource source = new FileDataSource(path);
+				bodyPart.setDataHandler(new DataHandler(source));
+				bodyPart.setFileName(path);
+				multipart.addBodyPart(bodyPart);
+			}
+			mail.setContent(multipart);
+			Transport.send(mail);
+			debug(this, "Repport sent");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
