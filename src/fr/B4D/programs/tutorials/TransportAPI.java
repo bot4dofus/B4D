30750f3a968348238038151aae81c587249f9471
LucBerge <lucas.bergeron@outlook.fr>
2019-02-04 23:23:45 +0100

    Add java doc

--- src/fr/B4D/programs/tutorials/TransportAPI.java
+++ src/fr/B4D/programs/tutorials/TransportAPI.java
@@ -14,6 +14,15 @@ import fr.B4D.program.StopProgramException;
 import fr.B4D.transport.WrongPositionException;
 
 public final class TransportAPI {
+	
+	/** Ce tutoriel à pour objectif de mieux comprendre le fonctionnement et l'utilisation de l'API des transports.<br/>
+	 *  <br/>
+	 *  Fonctionnement :
+	 *  <ul>
+	 *  	<li>Se rend en (4, -18).</li>
+	 *  	<li>Se rend en (3, -19).</li>
+	 *  </ul>
+	 */
 	public final static Program TUTORIAL1 = new Program(Place.Aucun, Category.Tutorial, "Transport API", "Tutorial 1", null, null, new ProgramInterface() {
 		public void intro(Person person) {}
 		public void outro(Person person) {}
@@ -23,6 +32,13 @@ public final class TransportAPI {
 		}
 	});
 
+	/** Ce tutoriel à pour objectif de mieux comprendre le fonctionnement et l'utilisation de l'API des transports.<br/>
+	 *  <br/>
+	 *  Fonctionnement :
+	 *  <ul>
+	 *  	<li>Se rend au zaap enregistré par le joueur.</li>
+	 *  </ul>
+	 */
 	public final static Program TUTORIAL2 = new Program(Place.Aucun, Category.Tutorial, "Transport API", "Tutorial 2", null, null, new ProgramInterface() {
 		public void intro(Person person) {}
 		public void outro(Person person) {}
