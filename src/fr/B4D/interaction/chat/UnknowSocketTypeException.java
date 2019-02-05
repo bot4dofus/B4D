650fb60c2f7b79c4afcff1eed9b7c90154aa9dbc
LucBerge <lucas.bergeron@outlook.fr>
2019-02-05 19:25:42 +0100

    Add java doc

--- src/fr/B4D/interaction/chat/UnknowSocketTypeException.java
+++ src/fr/B4D/interaction/chat/UnknowSocketTypeException.java
@@ -2,10 +2,16 @@ package fr.B4D.interaction.chat;
 
 import net.sourceforge.jpcap.util.HexHelper;
 
+/** Signal que le type de trame est inconnu.
+*
+*/
 public class UnknowSocketTypeException extends Exception {
 
 	private static final long serialVersionUID = 924080672225339433L;
 	
+	/** Constructeur de l'exception {@code UnknowSocketTypeException}.
+	 * @param data - Tableau d'octet représentant la trame complète.
+	 */
 	public UnknowSocketTypeException(byte[] data) {
 		super("Unknow socket type [" + HexHelper.toString(data) + "]");
 	}
