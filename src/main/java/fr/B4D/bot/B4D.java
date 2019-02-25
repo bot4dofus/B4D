50830324034d79f7e13b5fc18647208cb9b94419
LucBerge <lucas.bergeron@outlook.fr>
2019-02-25 21:31:54 +0100

    Compatible Linux et possibilité de ne pas proposer l'envoi du rapport d'erreur pour les B4DException

--- src/main/java/fr/B4D/bot/B4D.java
+++ src/main/java/fr/B4D/bot/B4D.java
@@ -22,7 +22,6 @@ import fr.B4D.interaction.Status;
 import fr.B4D.interaction.chat.Channel;
 import fr.B4D.program.Program;
 import fr.B4D.socket.SocketListener;
-import net.sourceforge.jpcap.capture.CaptureDeviceLookupException;
 import net.sourceforge.jpcap.capture.CaptureDeviceOpenException;
 import net.sourceforge.jpcap.capture.InvalidFilterException;
 
@@ -57,11 +56,10 @@ public final class B4D{
 	 * @throws B4DException Si une exception B4D est levée.
 	 * @throws ClassNotFoundException Si un problème de déserialisation survient.
 	 * @throws IOException Si impossible de créer les fichiers de configuration.
-	 * @throws CaptureDeviceLookupException Si aucun réseau n'est détecté.
 	 * @throws CaptureDeviceOpenException Si le réseau de capture ne peut pas être écouté.
 	 * @throws AWTException Si la configuration de l'ordinateur ne permet pas l'automatisation des périphériques
 	 */
-	public B4D() throws B4DException, ClassNotFoundException, IOException, CaptureDeviceLookupException, CaptureDeviceOpenException, AWTException {
+	public B4D() throws B4DException, ClassNotFoundException, IOException, CaptureDeviceOpenException, AWTException {
 		
 		/** LOGGER **/
 		logger = new Logger();
--- src/main/java/fr/B4D/bot/B4DException.java
+++ src/main/java/fr/B4D/bot/B4DException.java
@@ -6,17 +6,48 @@ public class B4DException extends Exception{
 
 	private static final long serialVersionUID = -2949855971863227080L;
 	
+	private boolean canSendRepport;
+	
 	/** Constructeur de l'exception {@code B4DException}.
+	 * Aucun rapport d'erreur ne pourra être envoyé.
+	 * Cela est identique à {@code B4DException(false)
 	 */
 	public B4DException() {
-    	super();
+    	this(false);
     }
 	
 	/** Constructeur de l'exception {@code B4DException}.
+	 * @param canSendRepport - {@code true} pour avoir la possibilité d'envoyer un rapport d'erreur, {@code false} sinon.
+	 */
+	public B4DException(boolean canSendRepport) {
+    	super();
+		this.canSendRepport = canSendRepport;
+    }
+	
+	/** Constructeur de l'exception {@code B4DException}.
+	 * Un rapport d'erreur pourra être envoyé.
+	 * Cela est identique à {@code B4DException(raison, true)
 	 * @param raison - Raison de l'exception.
 	 */
 	public B4DException(String raison)
     {
-    	super(raison);
+    	this(raison, true);
     }
+	
+	/** Constructeur de l'exception {@code B4DException}.
+	 * @param raison - Raison de l'exception.
+	 * @param canSendRepport - {@code true} pour avoir la possibilité d'envoyer un rapport d'erreur, {@code false} sinon.
+	 */
+	public B4DException(String raison, boolean canSendRepport)
+    {
+    	super(raison);
+		this.canSendRepport = canSendRepport;
+    }
+	
+	/** Retourne un boolean représentant si un rapport d'erreur peut être envoyé.
+	 * @return {@code true} si un rapport d'erreur peut être envoyé, {@code false} sinon.
+	 */
+	public boolean canSendRepport() {
+		return this.canSendRepport;
+	}
 }
