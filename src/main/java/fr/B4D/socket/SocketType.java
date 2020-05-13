1df328202c94556678b36f3fb7fe4e842979e795
Luc Berge <lucas.bergeron@outlook.fr>
2020-05-13 19:01:44 +0200

    Add HDV research and enclose and enclose opened socket types

--- src/main/java/fr/B4D/socket/SocketType.java
+++ src/main/java/fr/B4D/socket/SocketType.java
@@ -4,16 +4,23 @@ import java.util.Arrays;
 
 import fr.B4D.socket.parser.ChatMessageSocketParser;
 import fr.B4D.socket.parser.HDVItemViewSocketParser;
+import fr.B4D.socket.parser.HDVResearchSocketParser;
 
 public enum SocketType {
 	
 	HDV_ITEM_VIEW_SOCKET(new byte[]{0x59, (byte) 0xe1}, 1, HDVItemViewSocketParser.class),
-	
+	HDV_RESEARCH_15_SOCKET(new byte[]{0x5a, 0x15}, 1, HDVResearchSocketParser.class),
+	HDV_RESEARCH_16_SOCKET(new byte[]{0x5a, 0x16}, 2, HDVResearchSocketParser.class),
+
 	CHAT_MESSAGE_C5_SOCKET(new byte[]{0x0d, (byte) 0xc5}, 1, ChatMessageSocketParser.class),
 	CHAT_MESSAGE_CD_SOCKET(new byte[]{0x0d, (byte) 0xcd}, 1, ChatMessageSocketParser.class),
 	CHAT_MESSAGE_C9_SOCKET(new byte[]{0x0d, (byte) 0xc9}, 1, ChatMessageSocketParser.class),
 	CHAT_MESSAGE_C6_SOCKET(new byte[]{0x0d, (byte) 0xc6}, 2, ChatMessageSocketParser.class),
-	CHAT_MESSAGE_CE_SOCKET(new byte[]{0x0d, (byte) 0xce}, 2, ChatMessageSocketParser.class);
+	CHAT_MESSAGE_CE_SOCKET(new byte[]{0x0d, (byte) 0xce}, 2, ChatMessageSocketParser.class),
+
+	DD_ENCLOSE_OPEN_9D_SOCKET(new byte[]{0x5d, (byte) 0x9d}, 1, null),
+	DD_ENCLOSE_OPEN_9E_SOCKET(new byte[]{0x5d, (byte) 0x9e}, 2, null),
+	DD_ENCLOSE_OPEN_6E_SOCKET(new byte[]{0x5d, (byte) 0x6e}, 2, null);
 	
 	public static SocketType fromSocket(byte[] socket) {
 		byte[] id = Arrays.copyOfRange(socket, 0, 2);
