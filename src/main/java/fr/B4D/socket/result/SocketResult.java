854d4d8479e2bb0874e70eea6c94a994fb0f2251
Luc Berge <lucas.bergeron@outlook.fr>
2020-11-22 19:08:48 +0100

    Add date time value

--- src/main/java/fr/B4D/socket/result/SocketResult.java
+++ src/main/java/fr/B4D/socket/result/SocketResult.java
@@ -1,5 +1,7 @@
 package fr.B4D.socket.result;
 
+import java.time.LocalDateTime;
+
 /**
  * The {@code SocketResult} class represents the result of a socket. This is an abstract class.
  * @author Lucas
@@ -7,10 +9,21 @@ package fr.B4D.socket.result;
  */
 public class SocketResult {
 
+	private LocalDateTime dateTime;
+	
 	/**
 	 * Constructor of the socket result class.
 	 */
 	public SocketResult() {
 		super();
+		dateTime = LocalDateTime.now();
+	}
+
+	/**
+	 * Returns the date and time when the event occurred.
+	 * @return Date and time of the event.
+	 */
+	public LocalDateTime getDateTime() {
+		return dateTime;
 	}
 }
