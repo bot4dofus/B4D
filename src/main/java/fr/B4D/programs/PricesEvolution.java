6c3cd40d42ddc7acbf22105b4de524bccfa9de9a
Luc Berge <lucas.bergeron@outlook.fr>
2020-11-23 16:37:03 +0100

    Append the values to a googleSheet

--- src/main/java/fr/B4D/programs/PricesEvolution.java
+++ src/main/java/fr/B4D/programs/PricesEvolution.java
@@ -3,16 +3,20 @@ package fr.B4D.programs;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.MissingResourceException;
+
+import org.json.simple.JSONObject;
 
 import fr.B4D.bot.B4D;
 import fr.B4D.bot.B4DException;
 import fr.B4D.bot.Person;
 import fr.B4D.building.hdv.HDV;
+import fr.B4D.dofus.Dofus;
+import fr.B4D.dofus.items.Resource;
 import fr.B4D.google.GoogleSheet;
 import fr.B4D.program.CancelProgramException;
 import fr.B4D.program.Category;
@@ -20,6 +24,7 @@ import fr.B4D.program.Place;
 import fr.B4D.program.Program;
 import fr.B4D.socket.result.HDVItemViewSocketResult;
 import fr.B4D.socket.store.HDVItemViewSocketStore;
+import fr.B4D.utils.PointF;
 
 /**
  * The {@code PricesEvolution} class contains program relative to price evolution.
@@ -28,16 +33,6 @@ import fr.B4D.socket.store.HDVItemViewSocketStore;
  *
  */
 public final class PricesEvolution extends Program{
-
-	/**
-	 * The google credentials
-	 */
-	public static final String CREDENTIAL_FILE = "programs/pricesevolution.credentials";
-
-	/**
-	 * The bundle file.
-	 */
-	public static final String BUNDLE_FILE = "programs/pricesevolution";
 
 	/**
 	 * The google sheet id field in the bundle file.
@@ -56,15 +51,10 @@ public final class PricesEvolution extends Program{
 	@Override
 	public void intro(Person person) throws CancelProgramException {
 		String googleSheetId = null;
+		
 		try {
 			googleSheetId = getBundleFile().getString(GOOGLE_SHEET_ID_FIELD);
-			if(googleSheetId == null)
-				throw new CancelProgramException("The key \"" + GOOGLE_SHEET_ID_FIELD + "\" is missing in the file \"" + BUNDLE_FILE + "\".");
-
-			googelSheet = new GoogleSheet(googleSheetId, CREDENTIAL_FILE);
-		}
-		catch(MissingResourceException  e) {
-			throw new CancelProgramException("File \"" + BUNDLE_FILE + "\" is missing.");
+			googelSheet = new GoogleSheet(googleSheetId, getGoogleCredentials());
 		}
 		catch(IOException | GeneralSecurityException e) {
 			throw new CancelProgramException("Couldn't open the google sheet with id \"" + googleSheetId + "\". Either the credentials or the id is wrong.");
@@ -75,22 +65,44 @@ public final class PricesEvolution extends Program{
 	public void cycle(Person person) throws B4DException {
 		HDV.ASTRUB.open(person);
 
-		//clique sur le premier item
-
+		B4D.mouse.leftClick(new PointF(0.3289, 0.1959), false); //Select the first item
 
 		HDVItemViewSocketResult result;
+		Integer previousId = null;
+		List<HDVItemViewSocketResult> results = new LinkedList<HDVItemViewSocketResult>();
 		while((result = HDVItemViewSocketStore.getInstance().waitForResult(1000)) != null) {
-			List<List<Object>> values = new ArrayList<List<Object>>();
-			values.add(Arrays.asList(LocalDateTime.now().toString(), result.getPrice1(), result.getPrice10(), result.getPrice100()));
-			try {
-				googelSheet.write(values, "A1");
-			} catch (IOException e) {
-				B4D.logger.error(e);
+			if(result.getId().equals(previousId)) {
+				B4D.keyboard.sendKey(KeyEvent.VK_ENTER, 1000);
+				break;
 			}
-			B4D.keyboard.sendKey(KeyEvent.VK_DOWN);
+			B4D.wait.sleep(1000);
+			results.add(result);
+			previousId = result.getId();
+			B4D.keyboard.sendKey(KeyEvent.VK_ENTER, 1000);
+			B4D.keyboard.sendKey(KeyEvent.VK_DOWN, 1000);
+			B4D.keyboard.sendKey(KeyEvent.VK_ENTER, 1000);
 		}
 
-		B4D.wait.sleep(60000);
+		
+		JSONObject database = Dofus.getInstance().getDatabase().loadDatabase();
+		List<List<Object>> values = new ArrayList<List<Object>>();
+		for(HDVItemViewSocketResult r:results) {
+			Resource resource = Dofus.getInstance().getDatabase().findItemByKey("resources", "id", r.getId().toString(), database);
+			if(resource != null) {
+				String formatedDate = r.getDateTime().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+				values.add(Arrays.asList(formatedDate, resource.getName(), r.getPrice1(), r.getPrice10(), r.getPrice100()));
+			}
+			else
+				B4D.logger.warning("Couldn't find ressource with id " + r.getId().toString() + " in the database.");
+		}
+		
+		try {
+			googelSheet.append(values, "A1");
+		} catch (IOException e) {
+			B4D.logger.error(e);
+		}
+		
+		HDV.ASTRUB.close(person);
 	}
 
 	@Override
