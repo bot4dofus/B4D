52d1556370ff8468da4235a57aaf5a3b3c0f90b7
LucBerge <lucas.bergeron@outlook.fr>
2021-01-03 21:31:14 +0100

    Update latest-dofus-version.sh

--- devscripts/latest-dofus-version.sh
+++ devscripts/latest-dofus-version.sh
@@ -1,5 +1,6 @@
 link=$(curl -s https://www.dofus.com/fr/mmorpg/actualites/maj/correctifs | grep '<a href="/fr/mmorpg/actualites/maj/' | grep 'correctif-' | head -n 1)
 pre_version=${link#*'-correctif-'}
-end_index=$((${#pre_version}-2))
-version=${pre_version:0:${end_index}}
-echo ${version//-/.}
+#end_index=$((${#pre_version}-2))
+#version=${pre_version:0:${end_index}}
+#echo ${version//-/.}
+echo $pre_version
