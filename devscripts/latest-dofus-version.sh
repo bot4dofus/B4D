ebfa78a491d119a74b0ebd86e48374ac920fffbe
Luc Berge <lucas.bergeron@outlook.fr>
2021-08-24 20:28:11 +0200

    Fix script

--- devscripts/latest-dofus-version.sh
+++ devscripts/latest-dofus-version.sh
@@ -1,3 +1,4 @@
-link=$(curl -s https://www.dofus.com/fr/mmorpg/actualites/maj/correctifs | grep '<a href="/fr/mmorpg/actualites/maj/' | grep 'correctif-' | head -n 1)
+link=$(curl -s https://www.dofus.com/fr/rss/changelog.xml | grep 'https://www.dofus.com/fr/mmorpg/actualites/maj/' -m 1)
 version=$(echo ${link#*'-correctif-'} | cut -d'"' -f 1)
+version=${version%]]></link>}
 echo ${version//-/.}
